9941e9b99d801652cc0dbe0ca134baeb125a2351
任付伟 <1147907177@qq.com>
2019-10-23 20:24:36 +0800

    style: 清除多余代码

--- source/includes/_head.md
+++ source/includes/_head.md
@@ -1,4 +1,2 @@
 <!-- shins; content for the head element from source/includes/_head.md -->
 <!-- <meta name="theme-color" content="#F3F7F9"> -->
-<!-- ren增加head.md文件中内容 -->
-<!-- <div>我是头</div> -->
--- source/javascripts/app/_custom.js
+++ source/javascripts/app/_custom.js
@@ -4,25 +4,6 @@
 ;(function () {
     'use strict';
     // url信息
-    console.log("此js文件是放到index.html文件head元素中body的前面")
-    console.log(window.location);
-    var dom = $('.content > h1').text()
-    console.log(dom)
-    // function bind() {
-    //     content = $('.content');
-    //     searchResults = $('.search-results');
-    //
-    //     $('#input-search').on('keyup',function(e) {
-    //         var wait = function() {
-    //             return function(executingFunction, waitTime){
-    //                 clearTimeout(timeoutHandle);
-    //                 timeoutHandle = setTimeout(executingFunction, waitTime);
-    //             };
-    //         }();
-    //         wait(function(){
-    //             search(e);
-    //         }, searchDelay );
-    //     });
-    // }
+    console.log("此js文件是放到index.html文件head元素中body的前面,在加载文件之前就调用的js可以放这！")
 })();
 
